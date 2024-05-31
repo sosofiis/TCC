30bb6288638543fa2d1ce000fb70c1ac514f0976
Sofia <sofiaazleiria@gmail.com>
2024-05-31 15:35:52 -0300

    tcc atualização

---
+++ front/script.js
@@ -0,0 +1,110 @@
+async function postHumor(selecionado) {
+    let humor = selecionado;
+    let data = formatDate(new Date, 'aa-mm-dd'); //pesquisar como pegar o dia de hoje em javascript
+    let dados = {data, humor}
+
+    const response = await fetch('http://localhost:3000/api/store/humor', {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(dados)
+    });
+
+    let content = await response.json();
+
+    if(content.sucess) {
+        alert("Sucesso")
+    } else {
+        alert("Não");
+    }
+}
+
+async function postSintomas(selecionado) {
+    let sintomas = selecionado;
+    let data = formatDate(new Date, 'aa-mm-dd'); //pesquisar como pegar o dia de hoje em javascript
+    let dados = {data, sintomas}
+
+    const response = await fetch('http://localhost:3000/api/store/sintomas', {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(dados)
+    });
+
+    let content = await response.json();
+
+    if(content.sucess) {
+        alert("Sucesso")
+    } else {
+        alert("Não");
+    }
+}
+
+async function postAtiv_fisica(selecionado) {
+    let ativ = selecionado;
+    let data = formatDate(new Date, 'aa-mm-dd'); //pesquisar como pegar o dia de hoje em javascript
+    let dados = {data, ativ}
+
+    const response = await fetch('http://localhost:3000/api/store/ativ_fisica', {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(dados)
+    });
+
+    let content = await response.json();
+
+    if(content.sucess) {
+        alert("Sucesso")
+    } else {
+        alert("Não");
+    }
+}
+
+async function postTratamento(selecionado) {
+    let tratamento = selecionado;
+    let data = formatDate(new Date, 'aa-mm-dd'); //pesquisar como pegar o dia de hoje em javascript
+    let dados = {data, tratamento}
+
+    const response = await fetch('http://localhost:3000/api/store/tratamento', {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(dados)
+    });
+
+    let content = await response.json();
+
+    if(content.sucess) {
+        alert("Sucesso")
+    } else {
+        alert("Não");
+    }
+}
+
+async function postSono(selecionado) {
+    let sono = selecionado;
+    let data = formatDate(new Date, 'aa-mm-dd'); //pesquisar como pegar o dia de hoje em javascript
+    let dados = {data, sono}
+
+    const response = await fetch('http://localhost:3000/api/store/sono', {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(dados)
+    });
+
+    let content = await response.json();
+
+    if(content.sucess) {
+        alert("Sucesso")
+    } else {
+        alert("Não");
+    }
+}
+
+function formatDate(date, format) {
+    const map = {
+        mm: date.getMonth() + 1,
+        dd: date.getDate(),
+        aa: date.getFullYear().toString().slice(-2),
+        aaaa: date.getFullYear()
+    }
+
+    return format.replace(/mm|dd|aa|aaaa/gi, matched => map[matched])
+}
