2347a823347bd964abf6dc1bff4e088995fa5b3a
Sofia <sofiaazleiria@gmail.com>
2024-08-01 17:52:03 -0300

    att

--- front/calendario.js
+++ front/calendario.js
@@ -1,13 +1,19 @@
+//Onde fica escrito o mês, pega essa info
 const currentDate = document.querySelector(".current-date"),
+// Onde fica os dias
 daysTag = document.querySelector(".days"),
+//Botões de ir e voltar
 prevNextIcon = document.querySelectorAll(".icons span");
 
+// pega dia, mês e ano atual da biblioteca date do js
 let date = new Date(),
 currYear = date.getFullYear(),
 currMonth = date.getMonth();
 
+// lista dos meses
 const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
+// renderizar
 const renderCalendar = () => {
     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
@@ -21,8 +27,9 @@ const renderCalendar = () => {
     }
     
     for (let i = 1; i <= lastDateofMonth; i++) {
+        //verifica se i é o dia atual, se sim: ele cria uma class especial de que é hoje, se não: fds
         let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<li class="${isToday}">${i}</li>`;
+        liTag += `<li onClick="abrirDia(${i}, ${currMonth}, ${currYear})" class="${isToday}">${i}</li>`;
     }
 
     for (let i = lastDayofMonth; i < 6; i++) {
@@ -50,3 +57,17 @@ prevNextIcon.forEach(icon =>{
         renderCalendar();
     });
 });
+
+
+function abrirDia(dia, mes, ano) {    
+    //salvar no localStorage
+    localStorage.setItem('dia', dia);
+    localStorage.setItem('mes', mes);
+    localStorage.setItem('ano', ano);
+    
+    //redirecionar para a página dos sintomas registrados
+    window.location.href = './index.html'
+    
+    console.log(dia, mes, ano)
+    alert("funciono")
+}
